aa6ac053ca4ed6e8dc3b3d35115f50511de4cc6e
LAICHEEHOONG <laicheehoong@gmail.com>
2023-03-14 14:34:12 +0800

    try use math min
    try use math min

--- longest_common_prefix.js
+++ longest_common_prefix.js
@@ -1,14 +1,16 @@
 // Write a function to find the longest common prefix string amongst an array of strings.
 
 // If there is no common prefix, return an empty string "".
-
-function longestCommonPrefix(strs) {
+function longestCommonPrefix (strs) {
     if (strs.length === 0) return '';
-
     let prefix = strs[0];
     let resultArr = [];
-    let smallerLength = 0;
-
+    let resultArrLength = [];
+    let smallerLength;
+    resultArr.forEach(str => {
+        resultArrLength.push(str.length)
+    })
+    smallerLength = Math.min(...resultArrLength);
     strs.forEach(str => {
         for (let i = 0; i < prefix.length; i++) {
             if (prefix.substring(i, i + 1) === str.substring(i, i + 1)) {
@@ -18,24 +20,16 @@ function longestCommonPrefix(strs) {
             } else {
                 resultArr[i] = 0;
             }
-            if (smallerLength === 0) {
-                smallerLength = str.length;
-            } else if (smallerLength > str.length) {
-                smallerLength = str.length
-            }
         }
     })
     resultArr = resultArr.slice(0, smallerLength);
-
     const indexResultArr = resultArr.indexOf(0);
-
     if (indexResultArr !== -1) {
         let result = resultArr.slice(0, indexResultArr);
         return result.join('')
     } else {
         return resultArr.join('');
     }
-
 }
 
 
