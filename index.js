d807edc71dd66cb83a29319e40cf5b09490fc1a9
LAICHEEHOONG <laicheehoong@gmail.com>
2023-04-07 18:57:52 +0800

    3sum almost done
    3sum almost done

--- index.js
+++ index.js
@@ -1,183 +1,137 @@
-//12. Integer to Roman
+//15. 3Sum
 
-// create funtion roman1
-// roman1  Number convert to IIII, VIIII,XXXX, LXXXX
+const threeSum = (nums) => {
+    let compilation = allCompilation(nums);
+    let sumZero = filterSum(compilation);
+    let result = fileterSame(sumZero);
 
-// create funtion roman2
-// IIII=> IV, VIIII=> IX, XXXX, LXXXX
-// IIII = 4 ,VIIII = 9, 40 = XXXX, 90 = LXXXX, 400 = CCCC, 900 = DCCCC
-
-
-const symbol = {
-    M: 1000,
-    D: 500,
-    C: 100,
-    L: 50,
-    X: 10,
-    V: 5,
-    I: 1
+    return result;
 };
 
-// ['I','I',I','I'] => IV 
-// obj = {arr, num}
-const romanReduce = (obj) => {
-    let biger;
-    let small;
-    let smaller = [];
-    let {arr, num} = obj;
-    let result;
+// nums => [0, 1, -1, 0];
+// allCompilation(nums) => [[0,1,-1], ..., [1,0,0], ...]
 
-    for (let romanNum in symbol) { //romanNum: I, symbol[romanNum]: 1
-        if(num > symbol[romanNum]) {
-            biger = biger;
-        } else {
-            biger = romanNum;
+
+
+function allCompilation(arr) {
+    let loop = arr.length;
+    let compilation = [];
+    let three = [];
+    let second = 1;
+    let third = 2;
+
+    for (let i = 0; i < loop; i++) {
+        second = i + 1;
+        third = i + 2;
+
+        if (second > loop - 1) {
+            second = Math.abs(loop - second)
+        }
+        if (third > loop - 1) {
+            third = Math.abs(loop - third);
+        }
+        for (let j = 0; j < loop - 2; j++) {
+            three.push(arr[i]);
+            three.push(arr[second]);
+            three.push(arr[third]);
+
+            second++;
+            third++;
+
+            if (second > loop - 1) {
+                second = Math.abs(loop - second)
+            }
+            if (third > loop - 1) {
+                third = Math.abs(loop - third);
+            }
+
+            compilation.push(three);
+            three = [];
         }
     }
 
-    for(let romanNum in symbol) {
-        if(symbol[biger] > symbol[romanNum]) {
-            smaller.push(romanNum);
-        }
-    }
+    console.log(compilation);
+    return compilation;
+}
 
-    // console.log(`biger: ${biger}`);
-    // console.log(`smaller: ${smaller}`);
+//allCompilation([-1, 0, 1, 2, -1, -4]); //[0,1,-1]
 
-    smaller.forEach(roman => {
-        let cal = num - (symbol[biger] - symbol[roman]);
-        // console.log(cal)
-        if(cal === 0) {
-            small = roman;
+
+
+
+// filterSum(allCompilation(arr)) => [[0,1,-1], ...]
+function filterSum(arr) {
+    let filterArr = [];
+    arr.forEach(arr1 => {
+        let sum = 0;
+        arr1.forEach(num => {
+            sum = sum + num;
+        })
+        if (sum === 0) {
+            filterArr.push(arr1);
         }
     })
-
-    // console.log(`Small: ${small}`);
-
-    result = `${small}${biger}`;
-    // console.log(result);
-    return result;
-    
-
+    console.log(filterArr);
+    return filterArr;
 }
-// romanReduce({arr:['I','I','I','I'], num:4})
+// filterSum(allCompilation([-1, 0, 1, 2, -1, -4]))
 
-
-//  ['I','I',I','I'] find 4 repeat
-const filterRoman = (obj) => {
-    let arr = obj.arr;
+// fileterSame(filterSum(arr)) => [[0,1,-1], [2,-1, -1]]
+function fileterSame(arr) {
+    let arrBefore = arr;
     let result = [];
-    let ans;
-    
-    for(let i = 0; i < arr.length; i++) {
-        if(result.length === 0) {
-            result.push(arr[i]);
-        } else {
-            if(result[0] === arr[i]) {
-                result.push(arr[i]);
-            } else {
-                result = [arr[i]]
-            }
-        }
-    }
-
-    if(result.length === 4) {
-        // console.log(`active covert ${arr}`)
-        // console.log(obj)
-        return romanReduce(obj)
-
-        // return romanReduce(obj);
-        // return obj
-    } else {
-        // console.log(`no need covert ${arr}`)
-        ans = arr.join('');
-        // console.log(`Ans: ${ans}`);
-        // console.log(ans)
-        return ans;
-    }
-  
+    let sorted = sortArr(arr);
+    let match1 = findMatch(sorted);
+    return ''
 }
 
-// 3000 => ['M','M','M']; 4 => ['I','I','I','I']
-const nToR = (num) => {
-    let result = [];
-    let carry = num;
-    let run = true;
-
-    while (carry !== 0) {
-        run = true;
-        for (let romanNum in symbol) { //romanNum: I, symbol[romanNum]: 1
-            if (carry >= symbol[romanNum] && carry !== 0 && run) {
-                result.push(romanNum);
-                carry = carry - symbol[romanNum]
-                run = false;
-            }
-        }
-    }
-
-    // console.log(result);
-    return {
-        arr: result,
-        num: num
-    };
-}
+fileterSame(filterSum(allCompilation([-1, 0, 1, 2, -1, -4])));
 
 
-// ('3', 3) => 3000
-const strAddZero = (str, zero) => {
-    let arr = [str];
-
-    for (let i = 0; i < zero; i++) {
-        arr.push('0')
-    }
-    return Number(arr.join(''));
-}
-
-// 3999 => [3000, 900, 90, 9]
-const numToStrArr = (num) => {
-    let str = num.toString();
-    let arr = [];
-    let strLength = str.length - 1;
-
-    for (let i = 0; i < str.length; i++) {
-        // arr.push(str[i]);
-        let strToNum = strAddZero(str[i], strLength)
-        arr.push(strToNum);
-        strLength--;
-    }
-
-    return arr;
-};
-
-// [3000, 900, 90, 9] => ['MMM', 'CM', 'XC', 'IX'];
-const numToRoman = (arr) => {
-    let result = [];
-
-    arr.forEach(num => {
-        let r = filterRoman(nToR(num));
-        result.push(r);
+function sortArr(arr) {
+    let sorted = [];
+    arr.forEach(arr1 => {
+        let sort = arr1.sort((a, b) => a - b);
+        sorted.push(sort)
     })
-
-    return result;
-
-
+    console.log(sorted);
+    return sorted;
 }
 
-// ['MMM', 'CM', 'XC', 'IX'] => 'MMMCMXCIX' 
-const intToRoman = (num) => {
-    let numArr = numToStrArr(num);
-    let romanArr = numToRoman(numArr);
-    let result = romanArr.join('');
+function findMatch(arr) {
+    let ori = arr;
+    let cut = [];
+    let match = true;
+    let result = [];
 
-    console.log(result);
-    return result;
+        for (let i = 0; i < arr.length; i++) {
+            let compare = arr[i];
+            for (let j = 0; j < 3; j++) {
+                if (compare[j] === arr[0][j]) {
+                    if (match) {
+                        match = true;
+                    } else {
+                        match = false;
+                    }
+                } else {
+                    match = false;
+                }
+            }
     
-}
+            cut.push(match);
+            match = true;
+    
+        }
+    
+        for (let k = 0; k < arr.length; k++) {
+            if (!cut[k]) {
+                result.push(arr[k]);
+            }
+        }
+    
 
-intToRoman(4);
-intToRoman(9);
-intToRoman(40);
-intToRoman(90);
-intToRoman(400);
-intToRoman(900);
-intToRoman(3999);
+
+
+    console.log(result, 'result');
+
+}
